1d93be1dce804a70dd4a6fec6293d067014f284e
About Me <34916796+MahmoudAhmadOsman@users.noreply.github.com>
2020-02-24 15:40:47 -0600

    Update StudentsController.groovy

--- grails-app/controllers/org/schools/StudentsController.groovy
+++ grails-app/controllers/org/schools/StudentsController.groovy
@@ -2,17 +2,14 @@ package org.schools
 
 class StudentsController {
 
-	//static scaffold = Customer
-
 	static scaffold = Student
 
 	def index() {
-
+		
 		render(view: "index")
 
 		//1. Get all the info about Student from Student Model or Domain
 		def studentInfo = Student.list()
-
 
 		//2. Put a variable that will pass this info into the view
 		[studentInfo:studentInfo]
