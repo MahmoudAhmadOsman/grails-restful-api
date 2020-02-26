f25f014f12041b0d54d93658993e2db8adee4a9e
About Me <34916796+MahmoudAhmadOsman@users.noreply.github.com>
2020-02-26 10:52:48 -0600

    Update StudentsController.groovy

--- grails-app/controllers/org/schools/StudentsController.groovy
+++ grails-app/controllers/org/schools/StudentsController.groovy
@@ -11,7 +11,7 @@ class StudentsController {
 		//1. Get all the info about Student from Student Model or Domain
 		def studentInfo = Student.list()
 
-		//2. Put a variable that will pass this info into the view
+		//2. Create a variable that will pass this info into the view
 		[studentInfo:studentInfo]
 
 	}
