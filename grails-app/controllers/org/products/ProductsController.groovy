35cc0ee0bbb04ebed83a28a1e54acb8d8469bc03
About Me <34916796+MahmoudAhmadOsman@users.noreply.github.com>
2020-03-05 12:27:09 -0600

    Update ProductsController.groovy

--- grails-app/controllers/org/products/ProductsController.groovy
+++ grails-app/controllers/org/products/ProductsController.groovy
@@ -7,15 +7,13 @@ static scaffold = Products
     def index() {
    
 		//render(view: "index") // Custom view
-
 		// Get all products
+	    
 		def productList = Products.list()
 
 		//Query the data from the database
 		[productList:productList]
-		
-    
+		   
 }
-
 
 }
