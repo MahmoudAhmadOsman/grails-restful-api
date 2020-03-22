18820446e2d9285d28975eee21597aabde075aa1
About Me <34916796+MahmoudAhmadOsman@users.noreply.github.com>
2020-03-21 20:32:09 -0500

    Update ScholarshipsController.groovy

--- grails-app/controllers/org/schools/ScholarshipsController.groovy
+++ grails-app/controllers/org/schools/ScholarshipsController.groovy
@@ -3,6 +3,7 @@ package org.schools
 class ScholarshipsController {
 //This creates Dynamic Scaffolding (CRUD) for Scholarship
 	static scaffold  = Scholarships
+	// static belongsTo = [students:Students]
 	
 	def index() {
 
