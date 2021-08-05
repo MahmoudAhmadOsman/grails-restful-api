c1222d517aa6c177212a1954ad23463734d5d31c
Mahmoud Osman <34916796+MahmoudAhmadOsman@users.noreply.github.com>
2021-08-05 14:36:17 -0500

    Update AwardsController.groovy

--- grails-app/controllers/org/schools/AwardsController.groovy
+++ grails-app/controllers/org/schools/AwardsController.groovy
@@ -8,20 +8,11 @@ class AwardsController {
 	//ORM
 	static belongsTo = [students:Students, scholarships:Scholarships]
 	
-
 	def index() {
-
 		//render(view: "index")
 	}
-
  	def awards() {
  		render(view: "awards")
 		//redirect(controller: "scholarships", action: "index")
-	}
-		
-		
-	
-
-		
-		
+	}		
 }
