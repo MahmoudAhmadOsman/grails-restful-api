777285e89d35467dcec6e4dc264d89e6785271c0
Mahmoud Osman <34916796+MahmoudAhmadOsman@users.noreply.github.com>
2021-09-29 15:10:43 -0500

    Update ContactController.groovy

--- grails-app/controllers/org/contact/ContactController.groovy
+++ grails-app/controllers/org/contact/ContactController.groovy
@@ -4,30 +4,43 @@ class ContactController {
   def index = {
         redirect action: "list"
     }
+  
+  
     def create = {}
+  
     def save = {
         def contact = new Contact(params)
         contact.save flush: true, failOnError: true
         redirect action: "show", id: contact.id
     }
+  
+  
     def edit = {
         def contact = Contact.get(params.id)
         [contact: contact]
     }
+  
+  
     def update = {
         def contact = Contact.get(params.id)
         contact.properties = params
         contact.save flush: true, failOnError: true
         redirect action: "show", id: params.id
     }
+  
+  
     def show = {
         def contact = Contact.get(params.id)
         [contact: contact]
     }
+  
+  
     def list = {
         def contacts = Contact.list()
         [contacts: contacts]
     }
+  
+  
     def delete = {
         def contact = Contact.get(params.id)
         contact.delete flush: true, failOnError: true
